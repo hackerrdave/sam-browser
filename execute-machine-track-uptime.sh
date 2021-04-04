bd44e915aeecb3c7d2f1f4aca061f3c7db5a2ac6
Dave Kerr <davekerrcode@gmail.com>
2021-04-04 19:37:11 -0400

    Update execute-machine-track-uptime.sh

--- execute-machine-track-uptime.sh
+++ execute-machine-track-uptime.sh
@@ -14,16 +14,19 @@ echo "StepFunctions Local is Ready!"
 
 # Initialize StateMachine
 echo "creating state machine"
+
 states_json=$(cat ./statemachines/track-uptime.asl.json)
+
 aws stepfunctions --endpoint http://localhost:8083 create-state-machine \
     --name "TrackUptime" \
     --role-arn "arn:aws:iam::012345678901:role/DummyRole" \
     --definition "$states_json" \
     --no-cli-pager
+
 echo "state machine created"
+
 # Execute StateMachine
 aws stepfunctions --endpoint http://localhost:8083 start-execution \
     --state-machine-arn "arn:aws:states:us-east-1:123456789012:stateMachine:TrackUptime" \
     --input '{"url": "https://hackerrdave.com", "querySelector": "#___gatsby", "inverse": false}' \
     --no-cli-pager
-
